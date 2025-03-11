a3f6dcac8756ee733f28cee7fb5fe86c8db0394e
JA <jiaoao.jiao@alibaba-inc.com>
2025-03-11 10:05:17 +0800

    feat: 2.1 2.2学习

--- 2_预备知识/1.py
+++ 2_预备知识/1.py
@@ -1,4 +1,10 @@
-import tensorflow as tf
+import pandas as pd
 
-x = tf.range(12)
-print(x)
+data = pd.read_csv('../data/house_tiny.csv')
+print(data)
+
+inputs, outputs = data.iloc[:, 0:2], data.iloc[:, 2]
+inputs = inputs.fillna(inputs.mean())
+print(inputs)
+inputs = pd.get_dummies(inputs, dummy_na=True)
+print(inputs)
